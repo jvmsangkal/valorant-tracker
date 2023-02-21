68a69fd30fc4079c7ed5ea7ad1003ae1db60aae3
JV Sangkal <jvsangkal@thinkingmachin.es>
2023-02-21 15:51:26 +0800

    Setup eslint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,16 @@
-const path = require("path");
+const path = require('path')
 
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`;
+    .join(' --file ')}`
+
+const buildPrettierCommand = (filenames) =>
+  `yarn prettier --write ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')}`
 
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
-};
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand],
+  '*.{ts,tsx}': () => 'yarn tsc --noEmit',
+}
